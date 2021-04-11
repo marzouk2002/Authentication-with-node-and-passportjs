fb4cb21e6d4fb2d1289175b5a5e0f889044b907f
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-04-11 18:34:53 +0000

    register done

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://marzouk2002:ichou1242002@cluster0.8bzah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,43 @@
 const express = require('express')
 const router = express.Router()
 
+// Login page
 router.get('/login', (req, res) => {
     res.render('login')
 })
 
+// Rgister Page
 router.get('/register', (req, res) => {
     res.render('register')
 })
+
+// Register Handle
+router.post('/register', (req, res) => {
+    const { name, email, password, password2} = req.body
+    let errors = []
+
+    //Check required fields
+    if(!name || !email || !password || !password2) {
+        errors.push({msg : 'Please fill in all fields'})
+    }
+
+    //Check for password validation
+    if(password2 !== password) {
+        errors.push({msg: 'Sorry, passwords do not match'})
+    } 
+
+    //Check for password length
+    if(password.length < 6) {
+        errors.push({msg: 'Passwords shoold be at least 6 characters'})
+    } 
+
+    if(errors.length>0) {
+        res.render('register', { errors, name, email, password, password2})
+    } else {
+        res.send('pass')
+    }
+})
+
 
 
 module.exports = router;
